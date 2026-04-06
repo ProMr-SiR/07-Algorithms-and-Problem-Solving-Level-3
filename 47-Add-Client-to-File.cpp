bec0096c498308752d298c5305abe266f894395e
Pro_Mr_SiR <mgoihtahmumb@gmail.com>
2026-04-06 17:09:43 +0300

    Create 47-Add-Client-to-File.cpp

---
+++ 47-Add-Client-to-File.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct sClient
+{
+	string AcountNumber;
+	string PinCode;
+	string ClientName;
+	string ClientPhone;
+	double AccountBalance;
+};
+
+sClient ReadNewClient()
+{
+	sClient Client;
+
+	cout << "Adding New Client: \n\n";
+	// Usage of std::ws will extract allthe whitespace character
+	cout << "Enter Acount Number? ";
+	getline(cin >> ws, Client.AcountNumber);
+
+	cout << "Enter PinCode? ";
+	getline(cin, Client.PinCode);
+
+	cout << "Enter Client Name? ";
+	getline(cin, Client.ClientName);
+
+	cout << "Enter Client Phone? ";
+	getline(cin, Client.ClientPhone);
+
+	cout << "Enter Account Balance? ";
+	cin >> Client.AccountBalance;
+
+	return Client;
+}
+
+string ConvertRecordtoLine(sClient Client, string Seperator = "#//#")
+{
+	
+	string sClientRecord = "";
+
+	sClientRecord += Client.AcountNumber + Seperator;
+	sClientRecord += Client.PinCode + Seperator;
+	sClientRecord += Client.ClientName + Seperator;
+	sClientRecord += Client.ClientPhone + Seperator;
+	sClientRecord += to_string(Client.AccountBalance) + Seperator;
+
+	return sClientRecord;
+}
+
+void AddingDataToTheFile(string FileName,string stDataLine)
+{
+	fstream Myfile;
+	Myfile.open(FileName, ios::out | ios::app);
+
+	if (Myfile.is_open())
+	{	
+		Myfile << stDataLine << endl;
+
+		Myfile.close();
+	}
+}
+
+void AddNewClient()
+{
+	sClient Client;
+	Client = ReadNewClient();
+	AddingDataToTheFile("ClientsFileName.txt", ConvertRecordtoLine(Client));
+}
+
+void AddClients()
+{
+	sClient Client;
+	char PlayAgain = 'Y';
+
+	do
+	{
+		system("cls");
+		cout << "Adding New Client:\n\n";
+
+		AddNewClient();
+		cout << endl << "Client Added Successfully, Do you want to add more clients Y/N? ";
+		cin >> PlayAgain;
+	} while (PlayAgain == 'Y' || PlayAgain == 'y');
+}
+
+int main()
+{
+	
+	AddClients();
+
+	system("pause>0");
+
+	return 0;
+}
